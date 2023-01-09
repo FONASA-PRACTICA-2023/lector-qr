c4c3e2d8d1d4dff030efab60580ba8b55e168f9d
Your Name <you@example.com>
2023-01-09 10:57:29 -0300

    SJGDSJGDSD

--- src/pages/analizarQR/Muestreo.jsx
+++ src/pages/analizarQR/Muestreo.jsx
@@ -1,7 +1,11 @@
 import { useState, useRef, useCallback, useEffect } from "react";
 import QrReader from "react-web-qr-reader";
 import { FaQrcode } from 'react-icons/fa';
-import Alert from 'react-bootstrap/Alert';
+import { BiUserCircle } from 'react-icons/bi';
+import Cargando from "../../components/Cargando";
+import useApiSnoopy from "../../hooks/useApiSnoopy";
+import MensajeError from "../../components/MensajeError";
+import MensajeExito from "../../components/MensajeExito";
 
 
 const videoConstraintsFrontal = {
@@ -56,11 +60,10 @@ function Muestreo() {
   const [antecedentesSigges, setAntecedentesSigges] = useState({});
   const [casosAUGE, setCasosAUGE] = useState([]);
   const qrReaderRef = useRef(null);
-  const [showTaba, setShowTabla] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im5vbWJyZSI6Ik1pZ3VlbCBIZXJuXHUwMGUxbmRleiBHb256XHUwMGUxbGV6IiwicnVuIjoiTkEiLCJtYWlsIjoibWlndWVsLmhlcm5hbmRlekBmb25hc2EuZ292LmNsIiwidXNlcm5hbWUiOiJtaWd1ZWwuaGVybmFuZGV6IiwidGlwb191c3VhcmlvIjoiTkEiLCJydXRfcHJlc3RhZG9yIjoiIiwiaW5zdGl0dWNpb24iOiIiLCJyb2xlcyI6W119LCJpYXQiOjE2NzIzMjc0NjAsImV4cCI6MTY3MjMzMTA2MCwiaXNzIjoiRm9uZG8gTmFjaW9uYWwgZGUgU2FsdWQifQ.WKq6_MvycrMMd_I3gyvkjW0JeNV52IBEbIdaD2Kb5vA"
 
 
-  let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im5vbWJyZSI6Ik1pZ3VlbCBIZXJuXHUwMGUxbmRleiBHb256XHUwMGUxbGV6IiwicnVuIjoiTkEiLCJtYWlsIjoibWlndWVsLmhlcm5hbmRlekBmb25hc2EuZ292LmNsIiwidXNlcm5hbWUiOiJtaWd1ZWwuaGVybmFuZGV6IiwidGlwb191c3VhcmlvIjoiTkEiLCJydXRfcHJlc3RhZG9yIjoiIiwiaW5zdGl0dWNpb24iOiIiLCJyb2xlcyI6W119LCJpYXQiOjE2NzIzMjc0NjAsImV4cCI6MTY3MjMzMTA2MCwiaXNzIjoiRm9uZG8gTmFjaW9uYWwgZGUgU2FsdWQifQ.WKq6_MvycrMMd_I3gyvkjW0JeNV52IBEbIdaD2Kb5vA"
+  let apiSnoopy = useApiSnoopy();
 
 
   const callDatosPersonales = (rutd) => {
@@ -112,11 +115,14 @@ function Muestreo() {
         console.log(response.Beneficiarios.Beneficiario[0].CasosAUGE.CasoAUGE);
         setCasosAUGE(response.Beneficiarios.Beneficiario[0].CasosAUGE.CasoAUGE);
         setAntecedentesSigges(response);
+        if(response.Beneficiarios.Beneficiario[0].CasosAUGE.CasoAUGE.NombrePS==""){
+          console.log("hfdsaj")
+        }
         setLoading(false);
-
       })
       .catch(() => {
         console.log("error");
+        alert("EL USUARIO NO CUENTA CON DATOS AUGE O INTENTE NUEVAMENTE")
         setLoading(false);
       });
   };
@@ -148,15 +154,14 @@ function Muestreo() {
     setRutBuscado(rutd);
     callDatosPersonales(rutd);
     callDatosMedicos(rutd);
-
-
-
-    return rutd;
+  
   }
 
   function handleQrError(error) {
     console.error("QR error:", error);
   }
+  
+
 
   return (
     <div >
@@ -174,30 +179,30 @@ function Muestreo() {
           <button class="btn btn-outline-primary rounded " onClick={handleButtonClick} id="botnCap" style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", marginTop: "20px" }}><FaQrcode /> ESCANEAR QR</button>
         )}
       </div>
-
       <div className="container-tabla" style={{ marginTop: "20px", display: "none", height: "450px", overflowX: "scroll" }} id="fg" >
 
         <div className="card-body">
           <table class="table" style={{ marginTop: "20px" }}>
             <tbody>
+
               <ul class="list-group" style={{ background: "$blue" }}>
-                <li class="list-group-item active" aria-current="true" >Datos Afiliado</li>
+                <li class="list-group-item active" aria-current="true" ><BiUserCircle />Datos Afiliado</li>
                 <li class="list-group-item">Nombre: {datosPersonales.nombres}</li>
                 <li class="list-group-item">Apellidos: {datosPersonales.apellidoPaterno}<span>  </span>{datosPersonales.apellidoMaterno}</li>
                 <li class="list-group-item">Direccion: {datosPersonales.direccionPaciente}</li>
                 <li class="list-group-item">Comuna: {datosPersonales.glosaComuna}</li>
                 <li class="list-group-item">Rut: {rutBuscado}</li>
                 <li class="list-group-item">Sexo: {datosPersonales.sexo}</li>
+                
 
               </ul>
-
             </tbody>
           </table>
 
 
           {casosAUGE && casosAUGE.length > 0 && (
 
-            < table class="table table-borderless" style={{ marginTop: "20px" }}>
+            < table class="table table-borderless" style={{ marginTop: "20px" }} id="xsx">
               <thead >
 
                 <tr style={{ background: "#0f69b4", color: "white" }}>
@@ -207,10 +212,12 @@ function Muestreo() {
                   <td scope="row" >Estado de caso </td>
                   <td scope="row">Fecha de inicio </td>
                   <td scope="row">Fecha de termino</td>
+                
                 </tr>
               </thead>
 
               <tbody>
+
 
                 {casosAUGE.map(item => (
                   <tr key={item.FechaCreacion}>
@@ -220,15 +227,17 @@ function Muestreo() {
                     <td style={{ color: item.EstadoCaso === "Caso Cerrado" ? "red" : "green" }}>{item.EstadoCaso}</td>
                     <td> {item.FechaCreacion}</td>
                     <td>{item.FechaCierre}</td>
+                    
                   </tr>
-                ))}
 
+                ))}
               </tbody>
             </table>
           )}
         </div>
       </div>
     </div >
+
   );
 }
 
