4cd54cee622780d0ceadc08edc8ffcf047c44cd9
miguel.hernandez <miguel.hernandez@fonasa.cl>
2023-05-03 16:30:02 +0000

    dwe

--- src/pages/analizarQR/prueba.jsx
+++ src/pages/analizarQR/prueba.jsx
@@ -1,36 +1,34 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import QrReader from "react-web-qr-reader";
 import { FaQrcode } from 'react-icons/fa';
 
+const videoConstraintsTrasera = {
+  width: 350,
+  height: 350,
+  facingMode: { exact: "environment" },
+  focusMode: "continuous",
+  frameRate: 60,
+};
+
 function LectorQR() {
-  const [facingMode, setFacingMode] = useState("environment");
-  const [delay, setDelay] = useState(300);
+  
+
   const qrReaderRef = useRef(null);
 
-  const handleCameraToggle = () => {
-    if (facingMode === "environment") {
-      setFacingMode("user");
-    } else {
-      setFacingMode("environment");
-    }
-  };
-
   return (
-    <div>
+    <div >
       <div className="container-camara rounded d-print-inline-flex justify-content-center text-center mt-2" style={{ width: "100%" }}>
-        <QrReader
-          ref={qrReaderRef}
-          delay={delay}
-          videoConstraints={{ facingMode: facingMode }}
-          style={{ width: "100%", height: "100%" }}
-        />
-        <button className="btn btn-outline-primary rounded d-print-inline-flex mt-2 justify-content-center text-center " onClick={handleCameraToggle} id="botnCap" style={{ width: "100%" }}>
-          <FaQrcode className="mr-2" />
-          girar QR
-        </button>
+          <QrReader
+            delay={300}
+            ref={qrReaderRef}
+            videoConstraints={videoConstraintsTrasera}
+            style={{ width: "100%", height: "100%" }}
+          />
       </div>
-    </div>
+      
+      </div>
   );
 }
 
 export default LectorQR;
+
